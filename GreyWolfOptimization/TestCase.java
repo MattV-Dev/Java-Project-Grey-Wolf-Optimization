09d699fc49d3e10c344b0526cd1833ccc3e07614
MattV-Dev <mattv.dev@gmail.com>
2018-11-23 11:56:39 +0100

    Rearranged find minimum
    Rearranged the find minimum function and mirrored the find maximum
    function

--- GreyWolfOptimization/TestCase.java
+++ GreyWolfOptimization/TestCase.java
@@ -12,7 +12,7 @@ public class TestCase {
 			public double eval(List<Double> args) {
 				double wyn = 0;
 				for(Double d : args) {
-					wyn = Math.abs(1 / d) + Math.pow(Math.E, d) ;
+					wyn = Math.abs(d - 7.0);
 				}
 				return wyn;
 			}
@@ -21,11 +21,11 @@ public class TestCase {
 		List<Double> low = new ArrayList<Double>();
 		List<Double> upp = new ArrayList<Double>();
 		low.add(0.0);
-		upp.add(5.0);
+		upp.add(15.0);
 		
-		int N = 10;
+		int N = 20;
 		int D = 1;
-		int I = 30;
+		int I = 130;
 		
 		WolfPack pack = new WolfPack(N, D, low, upp);
 		Wolf solution = pack.findMinimum(f, I);
