4672b532c9c895903972df0295932bb57420459b
MattV-Dev <mattv.dev@gmail.com>
2018-12-11 12:27:10 +0100

    refactorised and simplified wolf pack's main functions
    findMinimum and findMaximum functions were split into smaller
    subfunctions, documented and recieved logged version each

--- GreyWolfOptimization/TestCase.java
+++ GreyWolfOptimization/TestCase.java
@@ -12,7 +12,7 @@ public class TestCase {
 			public double eval(List<Double> args) {
 				double wyn = 0;
 				for(Double d : args) {
-					wyn = Math.abs(d - 7.0);
+					wyn = (d - 1) * (d - 1) + Math.PI;
 				}
 				return wyn;
 			}
@@ -20,15 +20,15 @@ public class TestCase {
 		
 		List<Double> low = new ArrayList<Double>();
 		List<Double> upp = new ArrayList<Double>();
-		low.add(0.0);
-		upp.add(15.0);
+		low.add(-10.0);
+		upp.add(10.0);
 		
 		int N = 20;
 		int D = 1;
-		int I = 130;
+		int I = 30;
 		
-		WolfPack pack = new WolfPack(N, D, low, upp);
-		Wolf solution = pack.findMinimum(f, I);
+		WolfPack pack = new WolfPack();
+		Wolf solution = pack.findMinimum(f, I, N, D, low, upp);
 		
 		System.out.println("Solution: " + solution);
 		System.out.println("Value at solution: " + f.eval(solution.getPos()));
--- GreyWolfOptimization/Wolf.java
+++ GreyWolfOptimization/Wolf.java
@@ -1,12 +1,23 @@
 package GreyWolfOptimization;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Wolf {
 	private List<Double> pos;
 	
 	public Wolf(List<Double> pos) {
-		this.pos = pos;
+		this.pos = new ArrayList<Double>();
+		for(Double d : pos) {
+			this.pos.add((double) d);
+		}
+	}
+	
+	public Wolf(Wolf w) {
+		this.pos = new ArrayList<Double>();
+		for(Double d : w.getPos()) {
+			this.pos.add((double) d);
+		}
 	}
 	
 	public void setAtIndex(int i, double p) {
